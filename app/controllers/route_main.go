f1953f835ef9d8a43632067d21631a5ae74de7ab
mokori4242 <mokori4242@gmail.com>
2023-11-23 18:26:41 +0900

    [add]4-2:認証入る前のログインページ準備

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	i, err := template.ParseFiles("views/templates/index.html")
+	i, err := template.ParseFiles("views/templates/signup.html")
 
 	if err != nil {
 		log.Fatalln(err)
