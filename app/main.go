e14538f4a3a40258d0c10b70516d34846dbabb77
mokori4242 <mokori4242@gmail.com>
2023-11-24 13:21:15 +0900

    [refactor]main関数を整理

--- app/main.go
+++ app/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	// "fmt"
-	"app/config"
-	"app/models"
 	"app/controllers"
-	// "log"
-	"context"
 )
 
 func main() {
-	ctx := context.Background()
-	config.InitDB()
-	models.CreateUsersTable(ctx)
-	models.CreateTodosTable(ctx)
-	models.CreateSessionsTable(ctx)
 	controllers.StartMainServer()
 }
